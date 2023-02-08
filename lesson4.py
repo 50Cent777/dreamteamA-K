ca2b454a81a4c79ebf8dc0d5e7541434bacdb1e1
Елахунов Эдиль Назарович <maded@MacBook-Pro-Elahunov.local>
2023-02-08 20:58:19 +0600

    first commit

---
+++ lesson4.py
@@ -0,0 +1,67 @@
+#задача 1
+#for i in range(1, 1000):
+ #   if i % 2 == 0:
+  #      if i % 7 == 0:
+   #         print(i)
+
+#задача 2
+#str1 = 'abcdefghijklmnoprstuvwxyz'
+
+#print(str1[0:].title())
+
+
+#задача3
+
+health = 1000
+dream = 500
+lessons = 180
+sport = 100
+hw = 60
+cry = 200
+music = 60
+burger = 150
+
+#while health > 0:
+  #  action = input("выберите действие:")
+ #   if action == 'sport':
+      #  health -= sport
+       # print(f"Health: {health}")
+  #  if action == 'cry' :
+     #   health -= cry
+     #   print(f"Health: {health}")
+   # if action == 'dream':
+        #health += dream
+        #print(f"Health: {health}")
+    #if action == 'music':
+        #health -= music
+       # print(f"Health: {health}")
+    #if action == 'burger':
+      #  health += burger
+     #   print(f"Health: {health}")
+    #if action == 'hw':
+    #    health -= hw
+   #     print(f"Health: {health}")
+    #if action == 'lessons':
+  #      health -= lessons
+ #       print(f"Health: {health}")
+#else:
+#    print(f'Fatality! Health: {health}')
+
+
+#4)
+
+# incorrect_symbols = "*-#№?!@$%^()="
+# password1 = 'AAgwegwerg1541' #input("введите пароль:")
+# password2 = 'AAgwegwerg1541' #("Подтвердите пароль:")
+# digits = '0987654321ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+# isvalid = ''
+# while True:
+#     for i in password1:
+#         if i in digits:
+#             isvalid += i
+#     if password1 == password2:
+#         if len(password1)== len(isvalid):
+#             print('успех')
+#         else:
+#             print ('провал')
+
